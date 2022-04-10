d860dda8b3371819236951cba0ebceaef675e991
Maik Klein <maikklein@googlemail.com>
2022-04-10 22:02:55 +0200

    Move to submodule

--- example/RustExample/Source/RustExample/RustExample.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "RustExample.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RustExample, "RustExample" );
