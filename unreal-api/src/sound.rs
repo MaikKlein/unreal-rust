e4a2e713364e45ef0b65b11e66e58614a9518382
Maik Klein <maikklein@googlemail.com>
2022-08-20 12:55:21 +0200

    oops add missing sound file

---
+++ unreal-api/src/sound.rs
@@ -0,0 +1,21 @@
+use glam::{Vec3, Quat};
+use unreal_reflect::registry::USound;
+
+pub use crate::ffi::SoundSettings;
+use crate::module::bindings;
+
+pub fn play_sound_at_location(
+    sound: USound,
+    location: Vec3,
+    rotation: Quat,
+    settings: &SoundSettings,
+) {
+    unsafe {
+        (bindings().sound_fns.play_sound_at_location)(
+            sound.ptr,
+            location.into(),
+            rotation.into(),
+            settings,
+        );
+    }
+}
