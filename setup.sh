d052938b1bd9bee251eccd7db601176785cd61a0
Maik Klein <maikklein@googlemail.com>
2022-08-31 08:42:32 +0200

    Create folders

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 echo "Symlinking RustPlugin into RustExample"
+mkdir -p example/RustExample/Plugins
 ln -s ../../../RustPlugin example/RustExample/Plugins/RustPlugin
+mkdir -p example/RustExample/Binaries
